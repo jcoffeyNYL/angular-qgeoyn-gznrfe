b18ffed88f0dc24d24759dcf0da40fab2e77ed7b
jcoffeyNYL <73409496+jcoffeyNYL@users.noreply.github.com>
2023-05-09 17:18:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-qgeoyn-gznrfe
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-qgeoyn-gznrfe)
---
+++ app/select-multiple-example.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+/** @title Select with multiple selection */
+@Component({
+  selector: 'select-multiple-example',
+  templateUrl: 'select-multiple-example.html',
+  styleUrls: ['select-multiple-example.css'],
+})
+export class SelectMultipleExample {
+  toppings = new FormControl();
+  toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
+  selected: string[] = this.toppingList.filter((item, i) => i % 2 === 0);
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
